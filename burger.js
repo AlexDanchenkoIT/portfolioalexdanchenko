6ccf029d6eb82824ae7cf988f91576e299157a95
AlexDanchenko <foxpf@mail.ru>
2023-04-29 23:24:03 +0300

    init

---
+++ burger.js
@@ -0,0 +1,28 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME2 = 'body_fixed';
+
+
+const bodyNode2 = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content');
+const burgerCloseforNavigationNode = document.querySelector('.burger-nav');
+
+burgerCloseforNavigationNode.addEventListener("click", toggleBurger);
+
+burgerBtnNode.addEventListener("click", toggleBurger);
+
+burgerNode.addEventListener("click", (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME2);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
